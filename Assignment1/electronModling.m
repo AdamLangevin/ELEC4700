1e15d3dc4a22ce9a9428d0f03d5d99434db3fbcb
Adam Langevin <langer1911@gmail.com>
2018-01-19 10:01:17 -0500

    not done assg 1

--- Assignment1/electronModling.m
+++ Assignment1/electronModling.m
@@ -1,4 +1,4 @@
-function [ parts ] = electronModling( numElectrons )
+function [ parts ] = electronModling( numElectrons, T )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,24 +7,34 @@ if numElectrons == 0
     return
 end
 
-T = 300;
 Mo = 9.109 * 10 ^-31;
 Mn = 0.26*Mo;
-step = 2;
 k = 1.381 * 10 ^-23;
 vth = sqrt(3*k*T/Mn);
 
-%each Particle is a representation of each electrons starting position
-Particle = x:(0-200)*rand() 
-           y:(0-100)*rand()
-           xVel:(vth)*rand()
-           yVel:(vth)*rand()
+taumn = 0.2*e-12;
+
+L = 200*e-9;
+W = 100*e-9;
+
+xp(1,:) = linspace(0, L, numElectrons);
+yp(1,:) = linspace(0, W, numElectrons);
+
+x(numElectrons + 1:numElectrons) = xp - L/2;
+y(numElectrons + 1:numElectrons) = y(1) - W/2;
+
+% Each Particle is a representation of each electrons starting position
+% this is the important information, but this structure is difficult to use
+%Particle = x:(0-200)*rand() 
+%           y:(0-100)*rand()
+%           xVel:(vth)*rand()
+%           yVel:(vth)*rand()
            
 parts = {1,numElectrons};
 
-for p = 1:numElectrons
-    %update particle positions, and velocities
-     
+for i = 1:numElectrons
+    %initilize particle positions, and velocities
+    
 
 end
 
