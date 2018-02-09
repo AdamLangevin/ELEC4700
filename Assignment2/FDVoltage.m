d5ce023aaecded222c9969a9b6b25859c04c57a4
Adam Langevin <langer1911@gmail.com>
2018-02-09 13:20:04 -0500

    started Ass2

---
+++ Assignment2/FDVoltage.m
@@ -0,0 +1,65 @@
+clearvars
+
+L = 60;
+W = (2*L/3);
+
+G = sparse(L*W,L*W);
+
+V0 = 1;                     % 1V
+diff = 1;
+
+R = ones(W,L);
+B = zeros(1,W*L);
+
+%G matrix
+for j = 1:L     
+    for i = 1:W
+        n = i + (j-1)*W;    % I want this to march top left to right, top to bottom
+        
+        if i == 1           % left side
+            G(n,:) = 0;
+            G(n,n) = V0;
+            B(n) = V0;
+            
+        elseif i == W       % right side
+            G(n,:) = 0;
+            %G(n,n) = V0;
+            
+        elseif j == 1       % top
+            nxp = i + (j-2)*W;
+            nxn = i + j*W;
+            nyn = i + 1 + (j-1)*W;
+            
+            G(n,:) = 0;
+            %G(n,n) = V0;
+        elseif j == L       % bottom
+            nxp = i + (j-2)*W;
+            nxn = i + j*W;
+            nyp = i - 1 + (j-1)*W;
+            
+            G(n,:) = 0;
+            %G(n,n) = V0;
+        else                % interrior
+            nxp = i + (j-2)*W;
+            nxn = i + j*W;
+            nyp = i - 1 + (j-1)*W;
+            nyn = i + 1 + (j-1)*W;
+            
+            rxn = (R(i,j) + R(i+1))/2.0;
+            rxp = (R(i,j) + R(i-1,j))/2.0;
+            ryn = (R(i,j) + R(i,j+1))/2.0;
+            ryp = (R(i,j) + R(i,j-1))/2.0;
+            
+            G(n,n) = -(rxn+rxp+ryn+ryp);
+            G(n,nxp) = rxp;
+            G(n,nxn) = rxn;
+            G(n,nyn) = ryn;
+            G(n,nyp) = ryp;
+        end
+    end
+end
+figure(1);
+spy(G);
+
+V = G\B';
+
